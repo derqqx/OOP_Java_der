a8f5bd61169d3a213e30f21c702bf365eae806df
Adilzhan Zhumash <zhumash.adilzhan@gmail.com>
2024-12-15 13:08:22 +0500

    some changes

---
+++ Library.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+
+    private String libraryName;
+    private List<Book> books = new ArrayList<>();
+
+    public Library(String libraryName) {
+        this.libraryName = libraryName;
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void displayBooks() {
+        System.out.println("Books in " + libraryName + ":");
+
+        for (Book book : books) {
+            System.out.println(book);
+        }
+
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Library library = new Library("Cherie");
+
+        library.addBook(new Book("Jo Nesbo", "Rødstrupe", 2000));
+        library.addBook(new Book("Jo Nesbo", "Marekors", 2003));
+
+        library.displayBooks();
+
+        LibraryMember member = new LibraryMember(1001 , "Dancing Queen", library);
+        System.out.println("Library member: " + member);
+
+    }
+}
